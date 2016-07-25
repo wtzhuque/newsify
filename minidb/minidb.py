8e78bc1039e06a7caa957f458d28481ccee03f2c
Wang Tao <wtzhuque@163.com>
2016-07-25 11:02:17 +0800

    implement a mini db for links&docs

---
+++ minidb/minidb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: minidb.py
+Author: wtzhuque(wtzhuque@163.com)
+Date: 2016/07/25 11:00:15
+"""
+
+def main():
+    """
+    Main entry
+    """
+
+
+if __name__ == "__main__":
+    main()
+
