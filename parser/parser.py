62fdbbec2a16ef08d17aca514d8c4854be7defb6
Wang Tao <wtzhuque@163.com>
2016-08-01 20:16:41 +0800

    add parser and pipe

---
+++ parser/parser.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+File: parser.py
+Author: wangtao(wtzhuque@163.com)
+Date: 2016/08/01 17:52:45
+"""
+
+
+
---
+++ pipe/pipe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+"""
+File: pipe.py
+Author: wangtao(wtzhuque@163.com)
+Date: 2016/08/01 16:52:08
+"""
+
+
+def main():
+    """
+    Main Entry
+    """
+
+
+if __name__ == "__main__":
+    main()
+
